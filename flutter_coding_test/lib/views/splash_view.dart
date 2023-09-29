5c360c11fd98f5b8fd2f3c6bd993095f794aa813
naolgithub <naolcovered@gmail.com>
2023-09-29 23:10:45 +0300

    splash view file is modified

--- flutter_coding_test/lib/views/splash_view.dart
+++ flutter_coding_test/lib/views/splash_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_coding_test/controllers/splash_controllers.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashView extends GetView<SplashController> {
   const SplashView({Key? key}) : super(key: key);
@@ -12,9 +13,16 @@ class SplashView extends GetView<SplashController> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
+              Text(
                 'Bellboy',
-                style: TextStyle(fontSize: 50),
+                // style: TextStyle(fontSize: 50),
+                style: GoogleFonts.sacramento(
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic,
+                  textStyle: const TextStyle(
+                    fontSize: 44,
+                  ),
+                ),
               ),
               const SizedBox(
                 height: 15,
