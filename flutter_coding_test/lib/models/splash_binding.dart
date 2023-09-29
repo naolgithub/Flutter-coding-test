bb16cea82dd00394085d95b9a999940c98729106
naolgithub <naolcovered@gmail.com>
2023-09-29 22:25:55 +0300

    splash-binding file is modified

--- flutter_coding_test/lib/models/splash_binding.dart
+++ flutter_coding_test/lib/models/splash_binding.dart
@@ -1,6 +1,5 @@
+import 'package:flutter_coding_test/controllers/splash_controllers.dart';
 import 'package:get/get.dart';
-
-import 'splash_controller.dart';
 
 class SplashBinding extends Bindings {
   @override
